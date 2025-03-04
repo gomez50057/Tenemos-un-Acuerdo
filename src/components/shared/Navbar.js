719ad9f39c31b26bd139fd5cd2340726be091edb
gomez50057 <gomez.50057@gmail.com>
2025-03-04 16:48:21 -0600

    Navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,40 +1,17 @@
 "use client";
-import React, { useEffect, useState, useRef } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import './Navbar.css';
 
-const img = "/img/escudos/";
-const imgBasePath = "https://bibliotecadigitaluplaph.hidalgo.gob.mx/img_banco/";
+const img = "/img/";
 
 const Navbar = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [visible, setVisible] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [username, setUsername] = useState(''); // Supón que obtienes el nombre de usuario de algún lugar
-  const circuloRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      setVisible(currentScrollPos < scrollPosition || currentScrollPos < 10);
-      setScrollPosition(currentScrollPos);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [scrollPosition]);
-
-  const handleCirculoClick = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
-
   return (
-    <nav className={`Navbar ${visible ? 'active' : 'hidden'} ${scrollPosition> 100 ? 'scrolled' : ''}`}>
+    <nav className="Navbar">
       <ul>
         <div className="Navbar_img">
           <img src={`${img}Coordinación.png`} alt="Logo de Planeación" />
-          <img src={`/img/headertxt.png`} alt="Logo de Tenemos un Acuedo" />
+          <img src={`${img}headertxt.png`} alt="Logo de Tenemos un Acuedo" />
 
           <li><Link href="/"> Inicio </Link></li>
         </div>
@@ -44,8 +21,8 @@ const Navbar = () => {
             <li><Link href="/integrantes">Ejes</Link></li>
             <li><Link href="/noticias">Transversales</Link></li>
           </div>
-          <div className="Navbar_circulo" ref={circuloRef} onClick={handleCirculoClick}>
-            <img src={`${imgBasePath}estrella.webp`} alt="Estrella de Hidalgo" />
+          <div className="Navbar_circulo">
+            <img src={`${img}estrella.webp`} alt="Estrella de Hidalgo" />
           </div>
         </div>
       </ul>
