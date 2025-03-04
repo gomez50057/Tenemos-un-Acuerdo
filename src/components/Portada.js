051b21dff177cd7096daee537631430994c33be9
gomez50057 <gomez.50057@gmail.com>
2025-03-03 19:18:46 -0600

    portada

--- src/components/Portada.js
+++ src/components/Portada.js
@@ -1,10 +1,36 @@
+"use client";
+
+import { useEffect, useRef } from 'react';
 import styles from "../styles/Portada.module.css";
+const imgBasePath = "/img/";
 
 export default function Portada() {
+  const txtRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (txtRef.current) {
+        txtRef.current.classList.add(styles.fadeIn);
+      }
+      if (imgRef.current) {
+        imgRef.current.classList.add(styles.fadeIn);
+      }
+    },50);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <section className={styles.portada}>
-      <h2>Bienvenido a "Tenemos un Acuerdo"</h2>
-      <button>Acceder a la Consulta Digital</button>
+    <section id="header">
+      <div className={styles.contentHeader}>
+        <div className={`${styles.headerTxt} ${styles.fadeInTarget}`} ref={txtRef}>
+          <img src={`${imgBasePath}headertxt.png`} alt="img_representativa" />
+        </div>
+        <div className={`${styles.headerImg} ${styles.fadeInTarget}`} ref={imgRef}>
+          <img src={`${imgBasePath}headerimg.svg`} alt="img_representativa" className={styles.floatingImg} />
+        </div>
+      </div>
     </section>
   );
 }
