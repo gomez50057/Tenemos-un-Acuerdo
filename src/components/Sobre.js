1d8653610084ae9a3b844d2baa6608f5b214896f
gomez50057 <gomez.50057@gmail.com>
2025-03-03 19:33:02 -0600

    aboutTxt

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -2,13 +2,17 @@ import styles from "../styles/Sobre.module.css";
 
 export default function Sobre() {
   return (
-    <section className={styles.sobre}>
-      <h2>Un Plan para el Futuro de Hidalgo</h2>
-      <h3>Construido Contigo</h3>
+    <section className={styles.aboutContainer}>
+      <div className={styles.tren}>
+       
+      </div>
+      <div className={styles.aboutTxt}>
+        <h2>Un <span>Plan</span> para el <span className="spanDoarado">Futuro de Hidalgo</span> </h2>
+        <h3>Construido Contigo</h3>
+        <p>El Gobierno de Hidalgo, con una visión participativa y enfocada en el bienestar de la población, ha desarrollado el Plan Estatal de Desarrollo. Este plan se basa en cuatro ejes estratégicos que guían el crecimiento del estado, impulsando la prosperidad y el desarrollo sostenible en todas las regiones.</p>
+        <p>Además, incorpora tres temas transversales, fundamentales para garantizar que cada acción tenga un impacto positivo y duradero, promoviendo la equidad, la sostenibilidad y la innovación en todas las áreas del gobierno y la sociedad.</p>
+      </div>
 
-      <p>El Gobierno de Hidalgo, con una visión participativa y enfocada en el bienestar de la población, ha desarrollado el Plan Estatal de Desarrollo. Este plan se basa en cuatro ejes estratégicos que guían el crecimiento del estado, impulsando la prosperidad y el desarrollo sostenible en todas las regiones.</p>
-
-      <p>Además, incorpora tres temas transversales, fundamentales para garantizar que cada acción tenga un impacto positivo y duradero, promoviendo la equidad, la sostenibilidad y la innovación en todas las áreas del gobierno y la sociedad.</p>
     </section>
   );
 }
