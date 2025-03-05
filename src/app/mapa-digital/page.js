8e83c94ff32a3b34173e8685c591f1cd54ec532c
gomez50057 <gomez.50057@gmail.com>
2025-03-04 23:37:33 -0600

    Función para manejar el clic en la slide

--- src/app/mapa-digital/page.js
+++ src/app/mapa-digital/page.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ProjectMap from '@/components/maps/ProjectMap';
 import Navbar from '@/components/shared/Navbar';
-
+import Footer from '@/components/shared/Footer';
 
 const map = () => {
   return (
     <div>
       <Navbar />
       <ProjectMap />
-
+      <Footer />
     </div>
   );
 };
