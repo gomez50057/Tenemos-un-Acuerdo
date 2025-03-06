de3f1059c8d09be3a079f622038d510fbcd17cc1
gomez50057 <gomez.50057@gmail.com>
2025-03-05 22:17:03 -0600

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
-
+export const metadata = {
+  title: 'Tenemos un Acuerdo',
+  description: 'Descripción de mi aplicación',
+  viewport: 'width=device-width, initial-scale=1.0',
+};
 
 export default function Layout({ children }) {
   return (
