89c142333f9f9c0dfe50aaa1b5068719d5169b40
gomez50057 <gomez.50057@gmail.com>
2025-05-26 11:28:06 -0600

    planEstal

--- src/utils/planEstal.js
+++ src/utils/planEstal.js
@@ -3,7 +3,7 @@ export const ejesPlanEstatal = [
     slug: "/ejes-generales/eje-01",
     name: "_Eje 1 Estado_ *Planificado*, *Ordenado* y *Democrático*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Este eje busca construir un estado planeado escuchando la voz del pueblo para generar un desarrollo regional y político adecuado con base en la gobernanza. ",
     image: "/img/Ejes/Generales/eje1.png",
     linkEje: "/ejes-generales/eje-01"
   },
@@ -11,56 +11,56 @@ export const ejesPlanEstatal = [
     slug: "/ejes-generales/eje-02",
     name: "_Eje 2 Estado_ *Eficiente* y *Disciplinado* con sus *Finanzas*",
     description:
-     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+     "En este eje seguiremos transformando la administración pública a través de un Gobierno eficiente, responsable y honesto de los recursos públicos, que permita otorgar al pueblo un servicio de calidad y calidez humana.",
     image: "/img/Ejes/Generales/eje2.png",
   },
   {
     slug: "/ejes-generales/eje-03",
     name: "_Eje 3 Estado_  *Seguro* y *Justo*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "La seguridad del pueblo de Hidalgo es prioridad, por ello, este eje se enfoca en mejorar la calidad de la policía, garantizar la paz y una procuración de justicia imparcial con en estricto apego a los derechos humanos.",
     image: "/img/Ejes/Generales/eje3.png",
   },
   {
     slug: "eje-04",
     name: "_Eje 4 Estado_ *Fraterno* de *Bienestar* para el *Pueblo*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Este eje se enfoca en generar el bienestar para todas y todos los hidalguenses a través de servicios de salud de calidad, al igual que brindar una atención integral a todos los grupos vulnerables, la cual permita el pleno desarrollo de: las juventudes, las personas adultas mayores, las personas migrantes y los pueblos y comunidades indígenas.",
     image: "/img/Ejes/Generales/eje4.png",
   },
   {
     slug: "/ejes-generales/eje-05",
     name: "_Eje 5 Estado_ *Educado* y *Humanista*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Este eje reconoce la educación como un pilar fundamental para el progreso social, económico y cultural de nuestro estado, por ello, desarrollaremos un sistema educativo inclusivo, equitativo y de calidad, así como, el fomento al deporte en todos los sectores de la población.",
     image: "/img/Ejes/Generales/eje5.png",
   },
   {
     slug: "/ejes-generales/eje-06",
     name: "_Eje 6 Estado_ *Próspero* y de *Oportunidades*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Con este eje 6 Estado Próspero y de Oportunidades garantizaremos el crecimiento equitativo de Hidalgo, desarrollando más y mejores oportunidades de empleo para todas y todos, impulsando la industria existente y generando nuevas inversiones, también, apoyaremos el emprendimiento.",
     image: "/img/Ejes/Generales/eje6.png",
   },
   {
     slug: "/ejes-generales/eje-07",
     name: "_Eje 7 Estado_ *Orgulloso* de su *Cultura* y *Turismo*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Hidalgo tiene algo por ello, a través de las estrategias planteadas en el eje 7, consolidaremos el turismo como motor de desarrollo económico y social en nuestra entidad, así como, impulsaremos la identidad cultural, valorando y promoviendo las tradiciones, el patrimonio histórico y artístico con el que contamos.",
     image: "/img/Ejes/Generales/eje7.png",
   },
   {
     slug: "/ejes-generales/eje-08",
     name: "_Eje 8 Estado_ *Conectado* y con *Infraestructura* para el *Desarrollo*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Este eje se centra en garantizar una infraestructura moderna, eficiente y accesible con más y mejores caminos y carreteras, también, ampliaremos la cobertura de servicios básicos como son: agua, luz, drenaje y espacios de la vivienda. Impulsaremos el desarrollo urbano y el ordenamiento territorial para generar un desarrollo equilibrado y sostenible.",
     image: "/img/Ejes/Generales/eje8.png",
   },
   {
     slug: "/ejes-generales/eje-09",
     name: "_Eje 9 Estado_ *Sustentable* y *Productivo*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "A través de las estrategias y acciones establecidas en el eje 9, seguiremos transformando el sector agropecuario, asimismo, impulsaremos prácticas productivas que utilicen los recursos de manera eficiente, reduzcan la contaminación y generen el menor daño posible a los ecosistemas, promoviendo un desarrollo sustentable en Hidalgo.",
     image: "/img/Ejes/Generales/eje9.png",
   },
 ];
@@ -70,21 +70,21 @@ export const TransversalesPlanEstatal = [
     slug: "/ejes-trasversales/eje-01",
     name: "_Eje_ Transversal 1 _Estado_ *Transparente* y de *Rendición* de *Cuentas*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "La transparencia y la rendición de cuentas son pilares fundamentales para seguir recuperando la confianza del pueblo en el Gobierno, por ello, se garantizará a la población el acceso a la información pública, permitiéndoles estar mejor informados y participar de manera activa. Además, seguiremos combatiendo la corrupción, fomentando un gobierno que responda a las necesidades de la sociedad.",
     image: "/img/Ejes/Transversales/eje1.png",
   },
   {
     slug: "/ejes-trasversales/eje-02",
     name: "_Eje_ Transversal 2 _Estado_ *Tecnológico* e *Innovador*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "El eje transversal Estado Tecnológico e Innovador busca integrar la tecnología y la innovación en todos los ámbitos de la gestión pública, para ofrecer un servicio eficiente y de calidad a la ciudadanía. Además, impulsa el uso de las tecnologías, la digitalización, la investigación y la innovación como elementos claves para generar un desarrollo económico y social en Hidalgo.",
     image: "/img/Ejes/Transversales/eje2.png",
   },
   {
     slug: "/ejes-trasversales/eje-03",
     name: "_Eje_ Transversal 3 _Estado_ de *Igualdad Sustantiva* para las *Mujeres*",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eros nulla, facilisis ut congue eu, fringilla placerat felis. Mauris porttitor.",
+      "Este eje transversal busca transformar la sociedad en una en la que las mujeres puedan vivir de manera plena, libre de discriminación y con las mismas oportunidades que los hombres. La igualdad sustantiva, no solo beneficia a las mujeres, sino que favorece a toda la sociedad. También, garantizaremos los derechos que tienen las niñas, niños y adolescentes en Hidalgo.",
     image: "/img/Ejes/Transversales/eje3.png",
   },
 ];
