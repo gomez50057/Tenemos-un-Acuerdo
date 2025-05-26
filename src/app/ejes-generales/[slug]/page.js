8201603f6f5030d01d111d4faabf5dce925bfd6d
gomez50057 <gomez.50057@gmail.com>
2025-05-26 13:52:56 -0600

    jsconfig.json

--- src/app/ejes-generales/[slug]/page.js
+++ src/app/ejes-generales/[slug]/page.js
@@ -3,7 +3,10 @@
 import { useParams } from "next/navigation";
 import { ejesGenerales } from "@/utils/ejesGenerales";
 import EjeGeneral from '../../../components/ejeGeneral/EjeGeneral';
+<<<<<<< HEAD
 
+=======
+>>>>>>> 4d80f75 (prueba)
 
 export default function EjeGeneralPage() {
   const { slug } = useParams();
