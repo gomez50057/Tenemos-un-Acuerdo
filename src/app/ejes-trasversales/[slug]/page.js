06bcd16f38fbd5688e31c95d85fcef375e712c28
gomez50057 <gomez.50057@gmail.com>
2025-05-26 18:56:15 -0600

    ejes trasversales

--- src/app/ejes-trasversales/[slug]/page.js
+++ src/app/ejes-trasversales/[slug]/page.js
@@ -3,11 +3,21 @@
 import { useParams } from "next/navigation";
 import { ejesTransversales } from "@/utils/ejesTransversales";
 import Transversal from "@/components/ejeTransversal/EjeTransversal";
+import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 
 export default function TransversalPage() {
   const { slug } = useParams();
   if (!slug) return null;
 
   const transversal = ejesTransversales.find((item) => item.slug === slug);
-  return <Transversal transversal={transversal} />;
+
+  return (
+    <div>
+      <Navbar />
+      <Transversal transversal={transversal} />
+      <Footer />
+    </div>
+  );
+
 }
